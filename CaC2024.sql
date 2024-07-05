78ce523e42345a854e5522b634fef09c46bacc3c
Cerebro2005 <almeida_martin@hotmail.com>
2024-07-05 17:03:29 -0300

    Se Sube el Script de la BD

---
+++ CaC2024.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.22-MariaDB : Database - peliculas
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`peliculas` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `peliculas`;
+
+/*Table structure for table `pelicula` */
+
+DROP TABLE IF EXISTS `pelicula`;
+
+CREATE TABLE `pelicula` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `lanzamiento` date DEFAULT NULL,
+  `genero` varchar(100) DEFAULT NULL,
+  `duracion` time DEFAULT NULL,
+  `director` varchar(150) DEFAULT NULL,
+  `actores` varchar(150) DEFAULT NULL,
+  `sinopsis` varchar(1500) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
